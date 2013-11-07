32908848cc0ff89af53208d26c42de673e8138ab
dbutcher93 <dbutcher93@fullsail.edu>
2013-11-06 23:38:06 -0500

    ExpressionsIndustry

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,6 +1,7 @@
-var orderPrints = [150, 300, 75];
+var orderPrints = [200, 500, 150];
 var Orders = 3; 
 
 var Total = orderPrints[0] + orderPrints[1] + orderPrints[2];
 var Average = Total / Orders
+
 console.log(Average)
