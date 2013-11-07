ccd4bdcf2906e980c9b3b15cd3526786086959ca
dbutcher93 <dbutcher93@fullsail.edu>
2013-11-07 00:17:51 -0500

    ExpressionsIndustry

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -4,4 +4,4 @@ var Orders = 3; // Number of orders, based on number of different order prints
 var Total = orderPrints[0] + orderPrints[1] + orderPrints[2]; // Total pages printed
 var Average = Total / Orders // Average of printed pages
 
-console.log(Average) // Displays the average
+console.log("The average number of pages printed is " + Average + "") // Displays the average
