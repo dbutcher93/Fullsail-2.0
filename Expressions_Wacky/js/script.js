eeeeada6df131e1ac62196bf5c6eeff72b609290
dbutcher93 <dbutcher93@fullsail.edu>
2013-11-07 00:14:37 -0500

    ExpressionsWacky

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,6 +1,6 @@
-var Zombies = 30;
-var zombiesPerminute = 5;
-var timePassed = 45;
+var Zombies = 30; // Number of zombies in area
+var zombiesPerminute = 5; // Number of zombies that come to area
+var timePassed = 45; // time passed in area
 
-var totalZombies = zombiesPerminute * timePassed + Zombies;
-console.log(totalZombies)
+var totalZombies = zombiesPerminute * timePassed + Zombies; // Total number of zombies in area after the time passed
+console.log(totalZombies) // Dispays total amount of zombies
