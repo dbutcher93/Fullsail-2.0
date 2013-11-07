d7ecf06a93334c0daf92ce6427d278a278927771
dbutcher93 <dbutcher93@fullsail.edu>
2013-11-06 23:18:17 -0500

    ExpressionsPersonal

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,6 +1,6 @@
-var Diapers = 17;
-var babyFood = 15;
-var Juice = 5;
+var Diapers = 17; // Cost of diapers
+var babyFood = 15; // Cost of baby food
+var Juice = 5; // Cost of Juice
 
-var babyGroceries = Diapers + babyFood + Juice;
-console.log(babyGroceries);
+var babyGroceries = Diapers + babyFood + Juice; // Cost of all baby groceries
+console.log(babyGroceries); //Displays cost of Groceries 
