9f47cb64a89b5e8b82994292a4b7b03d8bd11cd2
dbutcher93 <dbutcher93@fullsail.edu>
2013-11-06 23:42:43 -0500

    ExpressionsIndustry

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,7 +1,7 @@
-var orderPrints = [300, 500, 400];
-var Orders = 3; 
+var orderPrints = [300, 500, 400]; // Number of pages per order
+var Orders = 3; // Number of orders, based on number of different order prints
 
-var Total = orderPrints[0] + orderPrints[1] + orderPrints[2];
-var Average = Total / Orders
+var Total = orderPrints[0] + orderPrints[1] + orderPrints[2]; // Total pages printed
+var Average = Total / Orders // Average of printed pages
 
-console.log(Average)
+console.log(Average) // Displays the average
