3869736a8051132f5992ed9f6189f2783cc57cd2
dbutcher93 <dbutcher93@fullsail.edu>
2013-11-13 22:51:57 -0500

    ConditionalsIndustry

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -1,10 +1,13 @@
-var printerInk = True;
-var printerPaper = True;
+var printerInk = true;
+var printerPaper = false;
 
 if (printerPaper && printerPaper){
 
 	console.log("Go ahead and print the order.")
 } else if (printerPaper || printerInk){
 
-	console.log("")
+	console.log("You need to replace the ink or add paper.")
+} else {
+
+	console.log("You need ink and paper to print the order.")
 }
