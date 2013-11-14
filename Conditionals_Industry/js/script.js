9525f45505b554741d9f8cd4d9d56ae00ffdebf0
dbutcher93 <dbutcher93@fullsail.edu>
2013-11-13 22:47:40 -0500

    ConditionalsIndustry

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -1,4 +1,10 @@
 var printerInk = True;
 var printerPaper = True;
 
-if (printerPaper && printerPaper)
+if (printerPaper && printerPaper){
+
+	console.log("Go ahead and print the order.")
+} else if (printerPaper || printerInk){
+
+	console.log("")
+}
