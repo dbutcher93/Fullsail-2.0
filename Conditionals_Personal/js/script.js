ee8bc65a3c61de7559aa2c65a8c33d4247b2e272
dbutcher93 <dbutcher93@fullsail.edu>
2013-11-13 19:46:11 -0500

    ConditionalsPersonal

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -1,5 +1,5 @@
-var Steaks = 6;
-var steaksNeeded = 5;
+var Steaks = 6; // Number of steaks you have
+var steaksNeeded = 5; // Number of steaks you need
 
 
-(Steaks < steaksNeeded) ? console.log("You will need to get more steak") : console.log("You have enough steak")
+(Steaks < steaksNeeded) ? console.log("You will need to get more steak") : console.log("You have enough steak") // a ternary equation to see you you have enough steak
