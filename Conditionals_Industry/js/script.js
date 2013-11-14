9fd4811735522e4aabf60e45812213c11628f913
dbutcher93 <dbutcher93@fullsail.edu>
2013-11-13 23:12:56 -0500

    ConditionalsIndustry

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -1,13 +1,13 @@
-var printerInk = true;
-var printerPaper = true;
+var printerInk = true; // Variable showing if there is or isn't ink in the printer
+var printerPaper = true; // Variable showing if the printer has paper
 
-if (printerPaper && printerPaper){
+if (printerPaper && printerPaper){ // Start of the if equation
 
-	console.log("Go ahead and print the order.")
-} else if (printerPaper || printerInk){
+	console.log("Go ahead and print the order.") // If true dispay this in the log
+} else if (printerPaper || printerInk){ // Start of the else if
 
-	console.log("You need to replace the ink or add paper.")
-} else {
+	console.log("You need to replace the ink or add paper.") // If the else if is true display this to the log
+} else { // Start of the else
 
-	console.log("You need ink and paper to print the order.")
+	console.log("You need ink and paper to print the order.") // If the else if is true display this to the log
 }
