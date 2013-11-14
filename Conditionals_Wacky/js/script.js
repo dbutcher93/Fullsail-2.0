870ec3f37d1de6d33396f56acfc649da7199fb16
dbutcher93 <dbutcher93@fullsail.edu>
2013-11-14 00:34:47 -0500

    ConditionalsWacky

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -1,3 +1,10 @@
 var chocolateMilkcups = 5;
 var Children = 3; 
 
+if (chocolateMilkcups >= Children){
+
+	console.log("You have enough cups of chocolate milk for the children")
+} else {
+
+	console.log("You do not have enough cups of chocolate milk for the children")
+}
