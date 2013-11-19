b7bce494e16d15a51bb662ab8c94d144e10c2b73
dbutcher93 <dbutcher93@fullsail.edu>
2013-11-19 02:00:15 -0500

    FunctionalsPersonal

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -1,6 +1,7 @@
-bills(112, 317, 61, 59)
+bills(112, 317, 61, 59);
 
 function bills(insurance, carPayment, dish, internet){
 
-	
+	var total = insurance + carPayment + dish + internet;
+	console.log("Thge total amount of bills for the month is" + total +)
 }
