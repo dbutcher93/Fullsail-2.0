65d5e7f1fc6ff0f4322686f843b5e9821712b40e
dbutcher93 <dbutcher93@fullsail.edu>
2013-11-21 02:24:51 -0500

    functionsIndustry

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -1,4 +1,12 @@
-var functionName = function(){
+var drywall = function(){
 
-	
+	var Sheets = 5;
+	var panelsLeft = 4;
+
+	if (Sheets => panelsLeft){
+		console.log("You have enough sheets of drywall left to finish the job")
+	} else {
+
+		console.log("YOu do not even enough sheets to finish the job")
+	}
 }
