3f44d4b5ba020c1460e4e5bb8348e58de1d641a1
dbutcher93 <dbutcher93@fullsail.edu>
2013-11-21 03:36:35 -0500

    functionsIndustry

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -1,14 +1,14 @@
-var drywall = function(){
+var drywall = function(){ // start of anonymous function
 
-	var Sheets = 4;
-	var panelsLeft = 6;
+	var Sheets = 4; // number of drywall sheets
+	var panelsLeft = 6; / number of panels left that need drywall
 
-	if (Sheets >= panelsLeft){
-		console.log("You have enough sheets of drywall left to finish the job")
-	} else {
+	if (Sheets >= panelsLeft){ // start of if
+		console.log("You have enough sheets of drywall left to finish the job") // if true display this to log
+	} else { // start of else
 
-		console.log("You do not even enough sheets to finish the job")
+		console.log("You do not even enough sheets to finish the job") // if false display this to log
 	}
 }
 
-drywall();
+drywall(); // calls function to action
