c657831eb8eee5ce46ee2f7c42220ef409b2d000
dbutcher93 <dbutcher93@fullsail.edu>
2013-11-21 00:50:36 -0500

    functions

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -1,12 +1,10 @@
-console.log("---Personal---")
+Miles(20, 50, 10) // This show the number of miles driven each day
 
-Miles(20, 50, 10)
-
-function Miles(day1Miles, day2Miles, day3Miles) {
-	var Days = 3;
-	var totalMiles = day1Miles + day2Miles + day3Miles;
-	var Average = totalMiles / Days
-	console.log("The average number of miles is " + Average + " miles")
+function Miles(day1Miles, day2Miles, day3Miles) { // this shows the parameters for the function
+	var Days = 3; // this shows the number of days, this should be the number of arguments
+	var totalMiles = day1Miles + day2Miles + day3Miles; // this is where the miles are added up
+	var Average = totalMiles / Days // this is where the average is calculated
+	console.log("The average number of miles is " + Average + " miles") // this displays the average to the log
 }
 
-Miles();
+Miles(); // this calls the function to action
