095bf0685f387f9a18fee6e7c3e48dc84ee877bb
dbutcher93 <dbutcher93@fullsail.edu>
2013-11-21 03:34:32 -0500

    functionsWacky

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -1,8 +1,8 @@
-var Dogs = 1;
-var Cats = 4;
+var Dogs = 1; // number of dogs
+var Cats = 4; // number of cats
 
-var Pets = Dogs + Cats;
-var petsAllowed = 4;
+var Pets = Dogs + Cats; // finds out how many animals total
+var petsAllowed = 4; // number of pets allowed
 	
-var Permission = (Pets > petsAllowed) ? "You have to many pets" : "You do not have to many pets and can keep them all";
-console.log(Permission)
+var Permission = (Pets > petsAllowed) ? "You have to many pets" : "You do not have to many pets and can keep them all"; // tenary to see if you have to many pets or not
+console.log(Permission)// displays result to log
